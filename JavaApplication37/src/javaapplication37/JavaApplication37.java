41b451a3d88238a7ba32499eadfa58ad3bedbef3
juanjose202 <juanjosegomez1908@gmail.com>
2021-08-10 06:38:49 -0500

    Proyecto Incial

---
+++ JavaApplication37/src/javaapplication37/JavaApplication37.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication37;
+
+/**
+ *
+ * @author s107e12
+ */
+public class JavaApplication37 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
